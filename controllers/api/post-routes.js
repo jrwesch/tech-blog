901c0a0209735cbde215c966a8ed0ba1ad1f503b
jrwesch <jrwesch@gmail.com>
2023-04-19 14:09:37 -0400

    added models (Post, User, Comment)

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const { Project } = require('../../models');
-const withAuth = require('../../utils/auth');
+const { Post, User, Comment } = require('../../models');
+const sequelize = require('../../config/connection');
 
 router.post('/', withAuth, async (req, res) => {
   try {
